4e587511c15665101415bfe7e40e267d2bd3e282
shahab <shahabvshahabi1996@gmail.com>
2016-08-30 03:12:21 +0430

    Delete clock.js

--- clock.js
+++
@@ -1,9 +0,0 @@
-setInterval(function() {
-  function r(el, deg) {
-    el.setAttribute('transform', 'rotate('+ deg +' 50 50)')
-  }
-  var d = new Date()
-  r(sec, 6*d.getSeconds())  
-  r(min, 6*d.getMinutes())
-  r(hour, 30*(d.getHours()%12) + d.getMinutes()/2)
-}, 1)
